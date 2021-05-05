272983381a35fdd121c55896269e2050e41c73ae
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-05-05 17:37:09 -0600

    updating database

--- config/Database.php
+++ config/Database.php
@@ -6,30 +6,30 @@
     private $username = 'root';
     private $password = ''; */
     
-    private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    /* private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     private $db_name = 'l1q86343j7vkzl0r';
     private $username = 'b14glho9exaaatls';
-    private $password = 'dudegci2oe5zgx9s';
+    private $password = 'dudegci2oe5zgx9s'; */
     
     private $conn;
 
     // DB Connect
     public function connect() {
-        /* $url = getenv('mysql://b14glho9exaaatls:dudegci2oe5zgx9s@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l1q86343j7vkzl0r');
+        $url = getenv('mysql://b14glho9exaaatls:dudegci2oe5zgx9s@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l1q86343j7vkzl0r');
         $dbparts = parse_url($url);
-        
+
         $hostname = $dbparts['host'];
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
       
-        $dsn = "mysql:host={$hostname};dbname={$database}"; */
+        /*$dsn = "mysql:host={$hostname};dbname={$database}"; */
        
         $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        /* $this->conn = new PDO($dsn, $username, $password); */
+        /* $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); */
+        $this->conn = new PDO(`mysql:host={$hostname};dbname={$database}`, $username, $password);
 
         //catch error mode
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
