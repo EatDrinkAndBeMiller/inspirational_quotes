cccaf6b082750587b28a2ea1d172e18a6f777119
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-05-05 17:49:09 -0600

    updating database

--- config/Database.php
+++ config/Database.php
@@ -11,29 +11,28 @@
     private $username = 'b14glho9exaaatls';
     private $password = 'dudegci2oe5zgx9s'; */
     
-    protected $url = getenv('mysql://b14glho9exaaatls:dudegci2oe5zgx9s@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l1q86343j7vkzl0r');
-    protected $dbparts = parse_url($url);
-
-    protected $hostname = $dbparts['host'];
-    protected $username = $dbparts['user'];
-    protected $password = $dbparts['pass'];
-    protected $database = ltrim($dbparts['path'],'/');
-      
-    protected $dsn = "mysql:host={$hostname};dbname={$database}";
-
     private $conn;
 
     // DB Connect
     public function connect() {
-            
+        $url = getenv('mysql://b14glho9exaaatls:dudegci2oe5zgx9s@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l1q86343j7vkzl0r');
+        $dbparts = parse_url($url);
+
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+      
+        $dsn = "mysql:host={$hostname};dbname={$database}";
+       
         $this->conn = null;
 
       try { 
         /* $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); */
-        $this->conn = new PDO($this->dsn, $this->username, $this->password);
+        $this->conn = new PDO($dsn, $username, $password);
 
         //catch error mode
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        /* $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
