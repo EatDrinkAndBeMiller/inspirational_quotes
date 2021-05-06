4c295a4cd22c2dfef6c968ea1dbd09559618b89a
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-05-05 20:26:51 -0600

    updated db

--- config/Database.php
+++ config/Database.php
@@ -1,21 +1,10 @@
 <?php 
   class Database {
-/*     // DB Params (to connect to local host)
-    private $host = 'localhost';
-    private $db_name = 'quotes_final';
-    private $username = 'root';
-    private $password = ''; */
-    
-    /* private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    private $db_name = 'l1q86343j7vkzl0r';
-    private $username = 'b14glho9exaaatls';
-    private $password = 'dudegci2oe5zgx9s'; */
-    
     private $conn;
 
     // DB Connect
     public function connect() {
-        $url = getenv('mysql://b14glho9exaaatls:dudegci2oe5zgx9s@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l1q86343j7vkzl0r');
+        $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
 
         $hostname = $dbparts['host'];
@@ -28,11 +17,8 @@
         $this->conn = null;
 
       try { 
-        /* $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); */
         $this->conn = new PDO($dsn, $username, $password);
-
-        //catch error mode
-        /* $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
+        
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
@@ -40,3 +26,19 @@
       return $this->conn;
     }
   }
+
+  //catch error mode
+        /* $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
+        /* $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password); */
+
+
+        /*     // DB Params (to connect to local host)
+    private $host = 'localhost';
+    private $db_name = 'quotes_final';
+    private $username = 'root';
+    private $password = ''; */
+    
+    /* private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $db_name = 'l1q86343j7vkzl0r';
+    private $username = 'b14glho9exaaatls';
+    private $password = 'dudegci2oe5zgx9s'; */
